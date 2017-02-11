86556f0e4e8f825e47df96d9e4b03bbb2b61ac24
ldantz <lazare.dantz@etu.u-pec.fr>
2017-02-11 10:18:40 +0100

    Mise en commun

---
+++ R1/Client/src/com/socketClient/Emission.java
@@ -0,0 +1,36 @@
+package Cli;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+
+public class Emission implements Runnable {
+
+	private PrintWriter out;
+	private String login = null, message = null;
+	private Scanner sc = null;
+	
+	public Emission(PrintWriter out) {
+		this.out = out;
+		
+	}
+
+	
+	public void run() {
+		
+		  sc = new Scanner(System.in);
+		  
+		  while(true){
+			    System.out.println("Bonjour client. 1 pour liste des véhicules, 2 pour liste des employés.:");
+				message = sc.nextLine();
+				out.println(message);
+			    out.flush();
+			  }
+	}
+}
